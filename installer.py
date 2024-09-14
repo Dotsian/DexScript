36af6cac5e90652ac7d3a8bc655a1c7d129fe13e
DotZZ-alt <selbydarryl05@gmail.com>
2024-09-14 12:59:42 -0400

    Update installer.py

--- installer.py
+++ installer.py
@@ -1,1 +1,39 @@
-print("Installation Test")
+import base64
+import requests
+
+from ballsdex.settings import settings
+
+await ctx.send("Installing DexScript...")
+
+GITHUB = "https://api.github.com/repos/DotZZ-alt/DexScript/contents/dexscript.py"
+request = requests.get(GITHUB)
+
+if request.status_code != requests.codes.ok:
+  await ctx.send("Failed to fetch the DexScript.py file\nReport this issue to dot_zz on Discord.")
+  return
+
+request = request.json()
+content = base64.b64decode(request["content"])
+
+# Create the DexScript file.
+with open("ballsdex/core/dexscript.py", "w") as opened_file:
+  contents = content.decode("UTF-8")
+  opened_file.write(contents.decode("utf-8"))
+
+# Add the ability to load the DexScript cog to the bot.py file.
+with open("ballsdex/core/bot.py", "r") as opened_file_1:
+  lines = opened_file_1.readlines()
+  contents = ""
+
+  for line in lines:
+    contents += line
+  
+    if "from ballsdex.core.commands import Core" in line:
+      contents += "from ballsdex.core.dexscript import DexScript\n"
+    elif "await self.add_cog(Core(self))" in line:
+      contents += "        await self.add_cog(DexScript(self))\n"
+  
+  with open("ballsdex/core/bot.py", "w") as opened_file_2:
+    opened_file_2.write(contents)
+
+await ctx.send(f"DexScript has been installed.\nRestart your bot for DexScript to work.\nUse `{settings.prefix}about` to test it out!")
