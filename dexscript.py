49f3043ee70f7d040ae97bc94011664c5608b449
DotZZ-alt <selbydarryl05@gmail.com>
2024-09-14 01:24:02 -0400

    Add DexScript v0.1

---
+++ dexscript.py
@@ -0,0 +1,213 @@
+import discord
+import logging
+import os
+import re
+import enum
+from typing import TYPE_CHECKING
+
+from discord.ext import commands
+
+from ballsdex.core.models import Ball
+
+log = logging.getLogger("ballsdex.core.dexscript")
+
+if TYPE_CHECKING:
+    from .bot import BallsDexBot
+
+
+CORE_PATH = os.path.dirname(os.path.abspath(__file__))
+START_CODE_BLOCK_RE = re.compile(r"^((```py(thon)?)(?=\s)|(```))")
+
+METHODS = [
+    "UPDATE",
+    "REMOVE",
+    "DISPLAY"
+]
+
+
+class TOKENS(enum.Enum):
+    METHOD = "METHOD"
+    NUMBER = "NUMBER"
+    STRING = "STRING"
+
+
+class DexScriptParser():
+    """
+    Class used for parsing DexScript contents into Python code.
+    Ported over from DotZZ's DexScript Migrations JavaScript file.
+    """
+
+    def __init__(self, ctx: commands.Context, code: str):
+        self.code = code
+        self.fields = []
+        self.ctx = ctx
+
+    def format_class(self, field):
+        """
+        Returns a class's identifier. 
+        If there is a token attached to the class, it will exclude the token.
+
+        Parameters
+        ----------
+        field: str
+            The class you want to use.
+        """
+
+        return (field[0] if isinstance(field, tuple) else field)
+
+    def grab_token(self, line):
+        """
+        Grabs the token based on a string provided.
+
+        Parameters
+        ----------
+        line: str
+            The string you want to grab the token from.
+        """
+
+        token = TOKENS.STRING
+        
+        if self.format_class(line) in METHODS:
+            token = TOKENS.METHOD
+        elif isinstance(line, int) or isinstance(line, float):
+            token = TOKENS.NUMBER
+
+        return (line, token)
+    
+    def parse_code(self):
+        """
+        Parses a DexScript Migration file and converts it into a readable list.
+        """
+
+        tracked_field = None
+        class_name = ""
+        class_data = {}
+
+        for index, field in enumerate(self.fields):
+            previous_field = self.fields[index - 1]
+
+            if previous_field is not None and previous_field[1] == TOKENS.METHOD:
+                tracked_field = previous_field
+
+            if tracked_field is None or tracked_field[1] != TOKENS.METHOD:
+                continue
+
+            formatted_class = self.format_class(tracked_field)
+
+            if class_name == "":
+                class_name = field[0]
+
+                try:
+                    class_data[formatted_class]
+                except Exception:
+                    class_data[formatted_class] = {}
+
+                class_data[formatted_class][class_name] = []
+
+            class_data[formatted_class][class_name].append(field[0])
+
+        return class_data
+
+    def parse(self, code: str):
+        if not "\n" in code:
+            code = "\n" + code
+
+        for line1 in code.split("\n"): 
+            if line1.startswith("//") or line1 == "" or line1 == "\n":
+                continue
+
+            for line2 in line1.split(" > "):
+                self.fields.append(self.grab_token(line2.replace("    ", "")))
+
+        print(self.fields)
+
+        return self.parse_code()
+    
+    async def execute(self, key, item):
+        formatted_ball = item["BALL"]
+        get_model = await Ball.get(country=formatted_ball[1])
+
+        match key:
+            case "UPDATE":
+                setattr(get_model, formatted_ball[2].lower(), formatted_ball[3])
+
+                await get_model.save()
+
+                await self.ctx.send(f"Updated `{get_model.country}'s` {formatted_ball[2]}")
+
+            case "REMOVE":
+                await get_model.delete()
+
+                await self.ctx.send(f"Deleted `{get_model.country}`")
+
+            case "DISPLAY":
+                await self.ctx.send(f"```{getattr(get_model, formatted_ball[2].lower())}```")
+
+    async def run(self):
+        code_fields = self.parse(self.code)
+
+        for key, field in code_fields.items():
+           await self.execute(key, field)
+
+        print(code_fields)
+
+
+class DexScript(commands.Cog):
+    """
+    DexScript support
+    """
+
+    def __init__(self, bot: "BallsDexBot"):
+        self.bot = bot
+
+    # TODO: Migrate this function over to the utilities path 
+    # so there aren't any duplicates.
+    @staticmethod
+    def cleanup_code(content): 
+        """
+        Automatically removes code blocks from the code.
+        """
+
+        if content.startswith("```") and content.endswith("```"):
+            return START_CODE_BLOCK_RE.sub("", content)[:-3]
+
+        return content.strip("` \n")
+
+    @commands.command()
+    @commands.is_owner()
+    async def run(self, ctx: commands.Context, *, code: str):
+        """
+        Executes DexScript code.
+        """
+
+        body = self.cleanup_code(code)
+
+        try:
+            dexscript_instance = DexScriptParser(ctx, body)
+            await dexscript_instance.run()
+        except Exception as e:
+            await ctx.send(f"```\n{e}\n```")
+        else:
+            await ctx.message.add_reaction("✅")
+
+    @commands.command()
+    @commands.is_owner()
+    async def about(self, ctx: commands.Context):
+        embed = discord.Embed(
+            title="DexScript - ALPHA",
+            description=(
+                "DexScript is a set of commands created by DotZZ that allows you to easily "
+                "modify, delete, and display data about balls.\n\n"
+                "For a guide on how to use DexScript, refer to the guide on the [DexScript GitHub Page]()"
+            ),
+            color = discord.Color.from_str("#03BAFC")
+        )
+
+        embed.set_thumbnail(url="https://i.imgur.com/uKfx0qO.png")
+        embed.set_footer(text="DexScript: v0.1")
+
+        await ctx.send(embed=embed)
+
+
+async def setup(bot: "BallsDexBot"):
+    await bot.add_cog(DexScript(bot))
