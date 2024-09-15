cc5c1e616c06760ef4ddf0964a91ed19bd33a0dd
Dotsian <selbydarryl05@gmail.com>
2024-09-15 04:41:06 -0400

    Add CF support to the uninstaller

--- uninstaller.py
+++ uninstaller.py
@@ -1,17 +1,19 @@
 import os
 
+dir_type = "ballsdex" if os.path.isdir("ballsdex") else "carfigures"
+
 await ctx.send("Uninstalling DexScript...")
 
-if os.path.isfile("ballsdex/core/dexscript.py"):
-  os.remove("ballsdex/core/dexscript.py")
+if os.path.isfile(f"{dir_type}/core/dexscript.py"):
+  os.remove(f"{dir_type}/core/dexscript.py")
 
 exclude = [
-  "from ballsdex.core.dexscript import DexScript",
+  f"from {dir_type}.core.dexscript import DexScript",
   "        await self.add_cog(DexScript(self))"
 ]
 
 # Add the ability to remove DexScript-related code from the bot.py file.
-with open("ballsdex/core/bot.py", "r") as opened_file_1:
+with open(f"{dir_type}/core/bot.py", "r") as opened_file_1:
   lines = opened_file_1.readlines()
   content = ""
 
@@ -21,7 +23,7 @@ with open("ballsdex/core/bot.py", "r") as opened_file_1:
 
     content += line
 
-  with open("ballsdex/core/bot.py", "w") as opened_file_2:
+  with open(f"{dir_type}/core/bot.py", "w") as opened_file_2:
     opened_file_2.write(content)
 
 await ctx.send("DexScript has been uninstalled.\nRestart your bot for DexScript to be removed.")
