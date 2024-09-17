6fff39186108fc787c6f510c2c07bd4cca281653
Dotsian <selbydarryl05@gmail.com>
2024-09-17 10:48:31 -0400

    Emoji fix

--- dexscript.py
+++ dexscript.py
@@ -154,9 +154,9 @@ class DexScriptParser():
         return_model = None
 
         if dir_type == "ballsdex":
-            return_model = await Ball.create(country=identifier)
+            return_model = await Ball.create(country=identifier, emoji_id = int(str("0") * 17))
         else:
-            return_model = await Ball.create(full_name=identifier)
+            return_model = await Ball.create(full_name=identifier, emoji_id = int(str("0") * 17))
 
         return return_model
 
