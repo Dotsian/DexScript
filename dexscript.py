a24f07b6d1184ca47628e5cc6a607d3cb87bfeda
Dotsian <selbydarryl05@gmail.com>
2024-12-12 12:34:49 -0500

    Remove packaging

--- dexscript.py
+++ dexscript.py
@@ -55,8 +55,6 @@ dexclasses = []
 dex_globals = {}
 dex_yields = []
 
-verified = False
-
 
 class Types(Enum):
     CLASS = 0
@@ -121,16 +119,10 @@ class Yield:
         )
 
 
-class Package:
-    def __init__(self, yml):
-        self.__dict__.update(yml)
-
-
 @dataclass
 class Settings:
     debug: bool = False
     outdated_warnings: bool = True
-    safe_mode: bool = True
     branch: str = "main"
 
     @property
@@ -577,23 +569,6 @@ class DexScriptParser:
 
         return (None, CodeStatus.SUCCESS)
 
-def fetch_package(name):
-    """
-    Returns package information based on the name passed.
-
-    Parameters
-    ----------
-    name: str
-        The name of the package you want to return.
-    """
-
-    package_path = f"{dir_type}/data/{name}.yml"
-
-    if not path.isfile(package_path):
-        return None
-
-    return Package(yaml_load(Path(f"{dir_type}/data/{name}.yml").read_text()))
-
 
 class DexScript(commands.Cog):
     """
@@ -677,41 +652,10 @@ class DexScript(commands.Cog):
 
     @commands.command()
     @commands.is_owner()
-    async def about(self, ctx: commands.Context, package: str = "DexScript"):
+    async def about(self, ctx: commands.Context):
         """
-        Displays information about a package.
-
-        Parameters
-        ----------
-        package: str
-            The package you want to view. Default is DexScript.
+        Displays information about DexScript.
         """
-
-        if package != "DexScript":
-            info = fetch_package(package)
-
-            if info is None:
-                await ctx.send("The package you entered does not exist.")
-                return
-
-            embed = Embed(
-                title=package,
-                description=info.description,
-            )
-
-            color = info.color if hasattr(info, "color") else "03BAFC"
-
-            embed.color = Color.from_str(f"#{color}")
-
-            if hasattr(info, "logo"):
-                embed.set_thumbnail(url=info.logo)
-
-            embed.set_footer(text=f"{package} {info.version}")
-
-            await ctx.send(embed=embed)
-
-            return
-
         embed = Embed(
             title="DexScript - BETA",
             description=(
@@ -732,161 +676,6 @@ class DexScript(commands.Cog):
         embed.set_footer(text=f"DexScript {__version__} ({version_check})")
 
         await ctx.send(embed=embed)
-
-    @commands.command()
-    @commands.is_owner()
-    async def uninstall(self, ctx: commands.Context, package: str):
-        """
-        Uninstalls a package.
-
-        Parameters
-        ----------
-        package: str
-            The package you want to uninstall.
-        """
-
-        embed = Embed(
-            title=f"Removed {package.title()}",
-            description=f"The {package.title()} package has been removed from your bot.",
-            color=Color.red(),
-            timestamp=datetime.now(),
-        )
-
-        rmtree(f"{dir_type}/packages/{package}")
-
-        await self.bot.unload_extension(f"{dir_type}/packages/{package}")
-        await self.bot.tree.sync()
-
-        await ctx.send(embed=embed)
-
-    @commands.command()
-    @commands.is_owner()
-    async def verify(self, ctx: commands.Context):
-        """
-        Verifies you want to install a package.
-        """
-
-        global verified
-
-        verified = True
-
-        await ctx.message.add_reaction("✅")
-
-    @commands.command()
-    @commands.is_owner()
-    async def install(self, ctx: commands.Context, package: str):
-        """
-        Installs a package to your Discord bot.
-
-        Parameters
-        ----------
-        package: str
-            The package you want to install.
-            It must be a GitHub link with a `package.yml` file.
-        """
-
-        global verified
-
-        if not package.startswith("https://github.com/"):
-            await ctx.send("The link you sent is not a valid GitHub link.")
-            return
-
-        if script_settings.safe_mode and not verified:
-            await ctx.send(
-                "**CAUTION:** are you sure you want to install this package?\n"
-                "All packages you install can modify your Discord bot.\n"
-                f"Run the `{settings.prefix}verify` comamnd to verify "
-                "you want to install this package.\n"
-                "-# To disable this warning, turn off the `safe_mode` setting."
-            )
-
-            return
-
-        if verified:
-            verified = False
-
-        t1 = time()
-
-        package_info = package.replace("https://github.com/", "").split("/")
-
-        original_name = package_info[1]
-
-        link = f"https://api.github.com/repos/{package_info[0]}/{package_info[1]}/contents/"
-
-        request = request_get(f"{link}package.yml")
-
-        if request.status_code == request_codes.ok:
-            content = b64decode(request.json()["content"])
-
-            with open(f"{dir_type}/data/{package_info[1]}.yml", "w") as package_file:
-                package_file.write(content.decode("UTF-8"))
-
-            yaml_content = yaml_load(content)
-            package_info = Package(yaml_content)
-
-            if dir_type not in package_info.supported:
-                await ctx.send(f"This package does not support {dir_type}.")
-                return
-
-            color = package_info.color if hasattr(package_info, "color") else "03BAFC"
-
-            embed = Embed(
-                title=f"Installing {original_name}",
-                description=(
-                    f"{original_name} is being installed on your bot.\n"
-                    "Please do not turn off your bot."
-                ),
-                color=Color.from_str(f"#{color}"),
-                timestamp=datetime.now(),
-            )
-
-            if hasattr(package_info, "logo"):
-                embed.set_thumbnail(url=package_info.logo)
-
-            original_message = await ctx.send(embed=embed)
-        else:
-            await ctx.send(
-                f"Failed to install {package_info[1]}.\n"
-                f"Report this issue to `{package_info[0]}`.\n"
-                f"```ERROR CODE: {request.status_code}```"
-            )
-            return
-
-        with suppress(FileExistsError):
-            mkdir(f"{dir_type}/packages/{package_info.name}")
-
-        for file in package_info.files:
-            file_path = f"{package_info.name}/{file}"
-            request_content = request_get(f"{link}{file_path}")
-
-            if request_content.status_code == request_codes.ok:
-                content = b64decode(request_content.json()["content"])
-
-                with open(f"{dir_type}/packages/{file_path}", "w") as opened_file:
-                    opened_file.write(content.decode("UTF-8"))
-            else:
-                await ctx.send(
-                    f"Failed to install the `{file}` file.\n"
-                    f"Report this issue to `{package_info.author}`.\n"
-                    f"```ERROR CODE: {request_content.status_code}```"
-                )
-
-        try:
-            await self.bot.load_extension(f"{dir_type}.packages.{package_info.name}")
-        except commands.ExtensionAlreadyLoaded:
-            await self.bot.reload_extension(f"{dir_type}.packages.{package_info.name}")
-
-        t2 = time()
-
-        embed.title = f"{original_name} Installed"
-
-        embed.description = (
-            f"{original_name} has been installed to your bot\n{package_info.description}"
-        )
-
-        embed.set_footer(text=f"{original_name} took {round((t2 - t1) * 1000)}ms to install")
-
-        await original_message.edit(embed=embed)
 
     @commands.command(name="update-ds")
     @commands.is_owner()
